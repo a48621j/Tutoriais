51f0dd9e588c9ad2159326b8fdae69a1b7be43e0
a48621j <alexandreabreu2001@gmail.com>
2022-12-06 01:19:47 +0000

    Vector part 1
    done

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -2,14 +2,14 @@
 
 public class Move : MonoBehaviour {
 
-    Vector3 goal = new Vector3(5, 0, 4);
+    public Vector3 goal = new Vector3(5, 0, 4);
+    public float speed = 0.1f;
 
     void Start() {
-
-        this.transform.Translate(goal);
+        goal = goal * 0.01f;
     }
 
     void Update() {
-
+        this.transform.Translate(goal.normalized * speed * Time.deltaTime);
     }
 }
