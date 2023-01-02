cd4a034991b2f93ac48617a8701564dd1160dd6e
a48621j <alexandreabreu2001@gmail.com>
2023-01-02 21:23:07 +0000

    a*
    resto do exercicio de A*

---
+++ Assets/AI_Controller.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI_Controller : MonoBehaviour
+{
+    public GameObject goal;
+
+    NavMeshAgent agent;
+
+    void Start()
+    {
+
+        agent = this.GetComponent<NavMeshAgent>();
+
+        agent.SetDestination(goal.transform.position);
+    }
+}
+
