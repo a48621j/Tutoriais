1f03af1593c0b0e53c247adb5705b30adffbd634
a48621j <alexandreabreu2001@gmail.com>
2023-01-02 19:31:12 +0000

    A* Pathfinding
    Inicio do exercício

---
+++ Assets/0_SimplePlan/Move.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public Vector3 goal = new Vector3(5, 0, 4);
+    public float speed = 0.1f;
+
+    void Start() {
+        goal = goal * 0.01f;
+    }
+
+    void Update() {
+        this.transform.Translate(goal.normalized * speed * Time.deltaTime);
+    }
+}
