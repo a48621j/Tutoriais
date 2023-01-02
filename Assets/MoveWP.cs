8aa29c77cd130ee52036181e609c6c9505c58ac0
a48621j <alexandreabreu2001@gmail.com>
2023-01-02 19:25:03 +0000

    Following Circuit
    Etapa feita

--- Assets/MoveWP.cs
+++ Assets/MoveWP.cs
@@ -6,25 +6,45 @@ public class MoveWP : MonoBehaviour
 {
     public GameObject[] waypoints;
     int currentWP = 0;
-
     public float speed = 10.0f;
     public float rotSpeed = 10.0f;
+    public float lookAhead = 10.0f;
+    GameObject tracker; 
     void Start()
     {
-        
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
     }
 
-    
-    void Update()
+    void ProcessTracker()
     {
-        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
+        if (Vector3.Distance(tracker.transform.position, this.transform.position) > lookAhead) return;
+
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3.0f)
+        {
             currentWP++;
+        }
 
         if (currentWP >= waypoints.Length)
+        {
             currentWP = 0;
-       
-        
-        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+        }
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0.0f, 0.0f, (speed + 20.0f) * Time.deltaTime);
+    }
+
+
+        void Update()
+    {
+        ProcessTracker();
+
+
+        //this.transform.LookAt(waypoints[currentWP].transform);
+
+        Quaternion lookatWP = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
 
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
 
